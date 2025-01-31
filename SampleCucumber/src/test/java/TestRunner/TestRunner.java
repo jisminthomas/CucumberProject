e046c91ed0b63263dbbd81b37e4e97176c280797
jismi <jismi@Jismin_Geo.home>
2025-01-30 23:05:53 -0600

    Adding Runner file

---
+++ SampleCucumber/src/test/java/TestRunner/TestRunner.java
@@ -0,0 +1,22 @@
+package TestRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		
+		features = {"src/test/resources/Feature/"},
+		glue = {"StepDefinitions", "MyHooks"},
+		plugin = { "pretty", "html:target/firstreport.html"}
+		
+		)
+
+public class TestRunner {
+
+}
